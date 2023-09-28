742a38a2fc315c12de7ea9edd349cbfc0187e06c
Andrea M Gamez <andreamgamez@gmail.com>
2023-09-27 17:03:39 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,77 @@
+// camelCase
+let SomeMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let myLibraryFunction
+
+// numeric literal expresion
+
+4.5
+
+//string literal expresion
+
+'This is Andrea'
+
+//Boolean literal expression
+
+true
+false
+
+//  complex / variable expressions.
+
+let todaysDate = 4 * (10/2) + 6
+let exampleTitle = !null
+
+// Declare (but do not assign) 9 variables for the following identifiers
+
+First_Name = firstName;
+Last_Name = lastName; 
+Address = address;
+City = city;
+State = state; 
+Zip_Code = zipCode 
+Your_Age = strYourAge 
+Referral_Source = strReferralSource;
+May_We_Contact_You = blnMayWeContactYou
+
+
+//Take the 3 of the 9 variables that you created above and demonstrate 3 ways for declaring and assigning values to those variables.
+
+let firstName = 'Andrea';
+let lastName = 'Diaz';
+let blnMayWeContactYou = true;
+
+//Add a number and a string and display the coerced result in the browser’s console window.
+
+let yearsStudying = 20
+alert('Mark has beeing stuying for ' + yearsStudying + ' years.')
+
+let hasDegree = true
+alert('Mark has a degree ' + hasDegree + ' in science.')
+
+//Is the following string literal valid? If not, how would you fix it?
+
+let someString = "Who once said, 'Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.'"
+console.log(someString)
+
+//Create a variable that produces a null value in the console window.create a variable that produces an undefined value in the console window.
+
+let y = null
+alert(y)
+
+let x
+alert(x)
+
+//Use the unary typeof operator on various literals to return the following types within the console window: string, number, Boolean, object, and undefined.
+
+let iceCream = {flavor:'Vanilla', size:'small'}
+
+let size = 10
+size.toString()
+
+size = Number('10')
+
+// STEP 1
+// STEP 2
+// STEP 3
